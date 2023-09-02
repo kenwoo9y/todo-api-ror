79a26545da8d52eb92ba9b35c68e3ace0463d0c5
kenwoo9y <kenwoo9y@gmail.com>
2023-09-02 23:44:39 +0900

    edit tasks controller

--- api/app/controllers/tasks_controller.rb
+++ api/app/controllers/tasks_controller.rb
@@ -1,13 +1,40 @@
 class TasksController < ApplicationController
   def index
+    tasks = Task.all.order(:id)
+    render json: tasks
   end
 
   def show
+    render json: @task
   end
 
-  def new
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: task
+    else
+      render json: task.errors
+    end
   end
 
-  def edit
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors
+    end
   end
+
+  def destory
+    if @task.destroy
+      render json: @task
+    else
+      render json: @task.errors
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name, :description, :status)
+    end
 end
