f1d9ae806de921d4200af9a6fbe0a1ee75ea650e
kenwoo9y <kenwoo9y@gmail.com>
2023-09-13 22:07:32 +0900

    edited user model

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+    
+    validates :user_name, presence: true, length: { maximum: 30, allow_blank: true }
+    validates :email, presence: true, length: { maximum: 80, allow_blank: true }
+    validates :password, presence: true, length: { minimum: 8 }, format: { with: VALID_PASSWORD_REGEX }, allow_nil: true
 end
